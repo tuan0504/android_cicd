d2d7798e10f249689ec7162c69338bc06d1aa439
tuan0504 <nguyennhattuan0504@gmail.com>
2022-02-17 10:26:39 +0700

    reset master

--- app/src/main/java/com/nn/architecture/features/weathers/respository/WeatherRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.nn.architecture.features.weathers.respository
-
-import androidx.lifecycle.LiveData
-import com.nn.architecture.core.api.Resource
-import com.nn.architecture.features.weathers.model.WeatherDailyDataModel
-
-interface WeatherRepository {
-    fun getWeathersDailyInCity(cityName: String): LiveData<Resource<List<WeatherDailyDataModel>>>
-}
